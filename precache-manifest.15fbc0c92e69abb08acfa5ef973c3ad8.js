9bec2e6d8c9b5cff927d223cc410c211da8698ac
Eishta <eishtamittal@gmail.com>
2020-05-21 09:00:33 +0530

    Updates

---
+++ precache-manifest.15fbc0c92e69abb08acfa5ef973c3ad8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dab6b991aebd1a742a8d",
+    "url": "/Locations/static/js/runtime~main.d2e87dbc.js"
+  },
+  {
+    "revision": "0f81b68780b4a9464b84",
+    "url": "/Locations/static/js/main.9fe0acfd.chunk.js"
+  },
+  {
+    "revision": "4790d35644e63e978c88",
+    "url": "/Locations/static/js/2.1cc5a2bd.chunk.js"
+  },
+  {
+    "revision": "0f81b68780b4a9464b84",
+    "url": "/Locations/static/css/main.add640c8.chunk.css"
+  },
+  {
+    "revision": "c5ca10efcad1e8206572f81093a597eb",
+    "url": "/Locations/index.html"
+  }
+];
